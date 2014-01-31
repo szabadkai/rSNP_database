21bc3698c43b5fe020441e1f6d84e8ad4f312ad0
szabadkai <szabadkai@abc.hu>
2014-01-31 08:39:41 +0100

    pnetek reggel

--- cgi-bin/print_rsnp_data.py
+++ cgi-bin/print_rsnp_data.py
@@ -9,7 +9,7 @@ con = mdb.connect('genome', 'rsnp', 'RSNP', 'testdb');
 
 form_data = cgi.FieldStorage().getvalue('SNPs')
 
-header_order = ['rs_ID','TFBS_ID','major_al', 'minor_al', 'freq_major', 'freq_min','rSNP_phastcons'] 
+header_order = ['rs_ID','TFBS_ID','major_al', 'minor_al', 'freq_major', 'freq_min','rSNP_phastcons','matrix_id'] 
 header={'rs_ID' :'SNP ID' ,'TFBS_ID' : 'TFBS' , 'freq_major':'F Major', 'freq_min':'F Minor','major_al':'MAJOR allele', 'minor_al':'MINOR allele','rSNP_phastcons' :'SNP phascons score','matrix_id':'M_ID'}
 
 print(yate.start_response())
@@ -29,11 +29,10 @@ with con:
     for row in rows:
         row['rs_ID']= "<a href='http://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?rs=%s'>%s</a><br>" % (row['RS_num'],row['RS_num'])
         row['TFBS_ID']="<a href='print_tfbs_data.py?id=%s'>%s</a></th>" % (row['TFBS_ID'],row['TFBS_ID'])
-
+        row['matrix_id']="<a href='print_matrix.py?id=%s'>show matrix</a>" % (row['matrix_id'])
         print "<tr>"
         for col in header_order:
             print "<th>%s</th>" % row[col]
         print "</tr>"
     print("</tr></table></div>")   
-    print "<br><a href='print_matrix.py?id=%s'>show matrix</a>" % form_data*'_1'
 print(yate.include_footer())
