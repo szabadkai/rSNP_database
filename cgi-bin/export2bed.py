7c2e8d7c98ec1a1159f231da0716b19b47377654
szabadkai <zeppike@gmail.com>
2014-02-12 15:12:10 +0100

    minor changes

--- cgi-bin/export2bed.py
+++ cgi-bin/export2bed.py
@@ -5,7 +5,6 @@ import yate
 con = mdb.connect('genome', 'rsnp', 'RSNP', 'testdb');
 
 header_order = ['chr','start','stop','peak']
-header = {'peak':'PEAK','chr':'chr','start':'start','stop':'stop'}
 
 form_data = cgi.FieldStorage().getvalue('exp')
 
@@ -14,21 +13,19 @@ form_data = cgi.FieldStorage().getvalue('exp')
 print(yate.start_response())
 
 with con: 
-    print '<div class="input_field"><table>'
+    print '<table>'
     cur = con.cursor(mdb.cursors.DictCursor)
-    cur.execute(""" SELECT * FROM TFBS,HTTP 
-                    WHERE TFBS.experiment='%s' AND 
-                    CONCAT_WS('_',TFBS.organism,TFBS.disease,TFBS.experiment)=HTTP.experiment order by chr ;""" % form_data)
+    cur.execute(""" SELECT * FROM TFBS 
+                    WHERE TFBS.experiment='%s' order by chr ;""" % form_data)
     rows = cur.fetchall()
 
     for row in rows:
         print "<tr>"
         for col in header_order:
             print "<th>%s</th>" % row[col]
-        print "<th><a href='%s'>LINK<a></th>" % row['http']    
         print "</tr>"
     else:
         pass
     
-    print("</table></div><br>")
+    print("</table>")
 print(yate.include_footer({""}))
