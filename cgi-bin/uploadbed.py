0c3f8c321f91361cc7a5b3a44a7db58aad141576
szabadkai <szabadkai@abc.hu>
2014-02-17 10:13:55 +0100

    ul

--- cgi-bin/uploadbed.py
+++ cgi-bin/uploadbed.py
@@ -23,7 +23,7 @@ def putToTmp(f):
 # processing the uploaded file
 form_data = cgi.FieldStorage()
 bedfile = form_data['bed']
-jaccard = float(form_data['jaccard'])
+#jaccard = float(form_data['jaccard'])
 
 tmpname = putToTmp(bedfile) # get the name of the tmp file
 
@@ -39,7 +39,7 @@ for bed in onlyfiles:
 	try:
 		a = BedTool(mypath+bed)
 		jac=BedTool.jaccard(userfile,a)
-		if jac['jaccard']>jaccard:
+		if jac['jaccard']>0.4:
 			print "%s\t%s\t%s\t%s<br>"% (bed,jac['intersection'],jac['union-intersection'],jac['jaccard'])
 	except:
 		print "problem in :"+bed
