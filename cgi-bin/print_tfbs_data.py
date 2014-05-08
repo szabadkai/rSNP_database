5edf831f93d896fc6a87359d16c9208c957d812c
szabadkai <szabadkai@abc.hu>
2014-05-08 14:36:42 +0200

    Gene added

--- cgi-bin/print_tfbs_data.py
+++ cgi-bin/print_tfbs_data.py
@@ -47,7 +47,9 @@ with con:
         pass
     
     print("</table></div><br>")
-    print("<a href='ortho_fasta.py?peak=%s' download='%s.fa'>download peak orthologs</a><br>" % (rows[0]['peak'],rows[0]['peak']) )
+   
+    #######################################################
+
 
     header_order = ['rs_ID','major_al', 'minor_al', 'freq_major', 'freq_min','rSNP_phastcons','orto_bases'] 
     header={'rs_ID' :'SNP ID' , 'freq_major':'F Major', 'freq_min':'F Minor','major_al':'MAJOR allele', 'minor_al':'MINOR allele','rSNP_phastcons' :'SNP phascons score','orto_bases':'Orthologs'}
@@ -73,16 +75,9 @@ with con:
         for col in header_order:
             print "<th>%s</th>" % row[col]
         print "</tr>"
-        
-    print("</table></div>")   
+    print("</table></div>")
 
-
-
-    cur.execute(""" SELECT * FROM TFBS,RS 
-                    WHERE TFBS.TFBS_ID='%s' AND 
-                    TFBS.TFBS_ID = RS.TFBS_ID""" % form_data)
-    rows = cur.fetchall()
-
+print("<a href='ortho_fasta.py?peak=%s' download='%s.fa'>download peak orthologs</a><br>" % (rows[0]['peak'],rows[0]['peak']) )
 print(yate.include_footer({""}))
 
 
