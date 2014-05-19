534befc4507a55f70eb8113e9dab2b30b84c22a0
szabadkai <szabadkai@abc.hu>
2014-05-19 14:49:41 +0200

    jQuery

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 
 <html>
-  <head>
+  <head>  
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
     <script src="//code.jquery.com/jquery-1.10.2.js"></script>
     <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
     <meta charset="utf-8">
