0dc3bf95eb9905327195895a537ce968976533ab
szabadkai <szabadkai@abc.hu>
2014-05-19 16:40:56 +0200

    jQueryq

--- index.php
+++ index.php
@@ -24,20 +24,15 @@
      <a href="http://emboss.abc.hu/rsnpdb/"><img src="logo2.png" alt="logo-rsnpdb" height="40"></a>
   </div>
 </div>
+<div><p id="sidebar"><?php echo file_get_contents('./DATA/sidebar.txt'); ?></p></div>
 
 <div id="accordion">
   <h3>SNP view</h3>
-      <div><p><div><p id="sidebar"><?php echo file_get_contents('./DATA/sidebar.txt'); ?></p></div>
-      <div class="container">
       <form action="./cgi-bin/print_rsnp_data.py" method="POST" enctype="application/x-www-form-urlencoded">
       Please let us know whitch SNP you're interested in? ( use rs# notation! )<br>
       <textarea id="seq" class="reset" rows="3" cols="80" name="SNPs">rs1000002 rs1000016 rs10000171 rs10000226  rs10000232</textarea><br>
       <input type=submit value="Select"></form>
-    </p>
-  </div>
   <h3>Experiment view</h3>
-  <div>
-    <p>
       <form action="./cgi-bin/print_exp_data.py" method="GET" enctype="application/x-www-form-urlencoded">
       <select id="exp" name="exp" >
       <?php
@@ -46,34 +41,24 @@
       foreach($rows as $row){ echo $row; } ?>
       </select> 
       <input type=submit value="Select"></form>
-  </p>
-  </div>
   <h3>BED view</h3>
-  <div>
-    <p>
       <form action="./cgi-bin/uploadbed.py" enctype="multipart/form-data" method="post">
       Upload you BED file to compare with other Chip-seq experiments.<br>
       <input type="file" id="bed" name="bed" value="Upload BED file"><br>
-
       <a href="#" title="The Jaccard index, also known as the Jaccard similarity coefficient 
       (originally coined coefficient de communauté by Paul Jaccard), is a statistic used for 
       comparing the similarity and diversity of sample sets.">Jaccard score</a>:
       <input type="text" id="jaccard" name="jaccard">
       <input type="submit" value="Upload and run analysis"><br><br><br>
       </form>
-    </p>
-  </div>
   <h3>Section 4</h3>
-  <div>
-    <p>
       <form action="./cgi-bin/lookup_gene.py" enctype="multipart/form-data" method="POST">
       Choose your gene of interest!<br>
       <input type="text" id="gene" name="gene">
       <input type="submit" value="submit">
       </form>
-    </p>
-  </div>
 </div>
-</div>
+
+
 </body></html>
 
