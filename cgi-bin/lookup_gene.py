56980c8d8b9030639deacdc065311eba89e3306a
szabadkai <szabadkai@abc.hu>
2014-05-20 10:42:09 +0200

    gene more info

--- cgi-bin/lookup_gene.py
+++ cgi-bin/lookup_gene.py
@@ -20,13 +20,13 @@ with con:
         
 
     cur = con.cursor(mdb.cursors.DictCursor)
-    cur.execute( "SELECT  *    FROM    TFBS,GENE2TFBS WHERE  GENE2TFBS.gene LIKE '%"+form_data.upper()+"%' AND  GENE2TFBS.TFBS_ID = TFBS.TFBS_ID " )
+    cur.execute( "SELECT  * FROM    HTTP,TFBS,GENE2TFBS WHERE  GENE2TFBS.gene LIKE '%"+form_data.upper()+"%' AND  GENE2TFBS.TFBS_ID = TFBS.TFBS_ID AND CONCAT_WS('_',TFBS.organism,TFBS.disease,TFBS.experiment)=HTTP.experiment ORDER BY TFBS.organism,TFBS.disease,TFBS.experiment" )
     rows = cur.fetchall()
     
     for row in rows:
         row['TFBS_ID']="<div id=\"%s\"><a onclick='tfbsdata(\"%s\")' href='print_tfbs_data.py?id=%s'target=\"_blank\">%s</a></div>" % (row['TFBS_ID'],row['TFBS_ID'],row['TFBS_ID'],row['gene'])
         print "<th>%s</th>" % row['TFBS_ID']
         print "<th>%s %s</th>" % (row['organism'],row['disease'])
-        print "<th>%s</th></tr>" % row['experiment']
+        print "<th><a href='%s'>LINK<a></th>" % row['http']
     print("</table></div>")   
 print(yate.include_footer(""))
