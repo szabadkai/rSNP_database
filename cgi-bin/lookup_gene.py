2e795e91133e28cf5c341bfdce7602f037d2e030
szabadkai <szabadkai@abc.hu>
2014-05-20 09:33:04 +0200

    gene more info

--- cgi-bin/lookup_gene.py
+++ cgi-bin/lookup_gene.py
@@ -27,6 +27,7 @@ with con:
         row['TFBS_ID']="<div id=\"%s\"><a onclick='tfbsdata(\"%s\")' href='print_tfbs_data.py?id=%s'target=\"_blank\">%s</a></div>" % ((row['TFBS_ID'],) * 4)
         print "<tr><th>%s</th>" % row['gene']
         print "<th>%s</th>" % row['TFBS_ID']
-        print "<th>%s</th></tr>" % row['homerpeaks_txt']
+        print "<th>%s %s</th>" % (row['organism'],row['disease'])
+        print "<th>%s</th></tr>" % row['experiment']
     print("</table></div>")   
 print(yate.include_footer(""))
