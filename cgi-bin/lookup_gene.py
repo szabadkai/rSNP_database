e7250dfb15218aa1180b78f7164cc5801aa93adf
szabadkai <szabadkai@abc.hu>
2014-05-20 09:36:39 +0200

    gene more info

--- cgi-bin/lookup_gene.py
+++ cgi-bin/lookup_gene.py
@@ -24,8 +24,7 @@ with con:
     rows = cur.fetchall()
     
     for row in rows:
-        row['TFBS_ID']="<div id=\"%s\"><a onclick='tfbsdata(\"%s\")' href='print_tfbs_data.py?id=%s'target=\"_blank\">%s</a></div>" % ((row['TFBS_ID'],) * 4)
-        print "<tr><th>%s</th>" % row['gene']
+        row['TFBS_ID']="<div id=\"%s\"><a onclick='tfbsdata(\"%s\")' href='print_tfbs_data.py?id=%s'target=\"_blank\">%s</a></div>" % ((row['TFBS_ID'],) * 3,row['gene'])
         print "<th>%s</th>" % row['TFBS_ID']
         print "<th>%s %s</th>" % (row['organism'],row['disease'])
         print "<th>%s</th></tr>" % row['experiment']
