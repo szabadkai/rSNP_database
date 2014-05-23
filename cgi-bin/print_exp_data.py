0fb9d4dd71c274033aaff17f0972967bad920f70
szabadkai <szabadkai@abc.hu>
2014-05-23 07:58:52 +0200

    refresh

--- cgi-bin/print_exp_data.py
+++ cgi-bin/print_exp_data.py
@@ -21,7 +21,7 @@ with con:
     cur = con.cursor(mdb.cursors.DictCursor)
     cur.execute(""" SELECT * FROM TFBS,HTTP 
                     WHERE TFBS.experiment='%s' AND 
-                    CONCAT_WS('_',TFBS.organism,TFBS.disease,TFBS.experiment)=HTTP.experiment order by chr;""" % form_data)
+                    CONCAT_WS('_','hs',TFBS.disease,TFBS.experiment)=HTTP.experiment order by chr;""" % form_data)
     rows = cur.fetchall()
 
     print "<thead>"
