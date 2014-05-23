b00ba172488eb10eeaa42e1c3191f2c47db3f3cf
szabadkai <szabadkai@abc.hu>
2014-05-23 13:43:49 +0200

    refresh

--- cgi-bin/print_tfbs_data.py
+++ cgi-bin/print_tfbs_data.py
@@ -32,7 +32,7 @@ with con:
                     WHERE TFBS_ID='%s' AND 
                     TFBS.peak = ORTHOLOGS.peak AND
                     CONCAT_WS('_',%s,TFBS.disease,TFBS.experiment)=HTTP.experiment 
-                    ;""" % (str(form_data),('hs' if row['organism'])==1 else 'Mus musculus'))
+                    ;""" % (form_data,'hs' if row['organism'])==1 else 'Mus musculus')
     rows = cur.fetchall()
     print "<thead>"
     for col in header_order:
