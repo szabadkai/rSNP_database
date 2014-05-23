1a8bd3bc91a428829bf6d66f38b680f2b05352ca
szabadkai <szabadkai@abc.hu>
2014-05-23 09:49:12 +0200

    refresh

--- cgi-bin/print_rsnp_data.py
+++ cgi-bin/print_rsnp_data.py
@@ -43,7 +43,7 @@ with con:
             if row['strand']=='-':
                 pos = row['start']-row['SNP_pos']
             else: 
-                pos = row['stop']-row['SNP_pos']
+                pos = row['SNP_pos']-row['start']
             row['matrix_id']="<a href='print_matrix.py?id=%s&pos=%s&minor=%s&major=%s'>show matrix</a>" % (row['rs_ID'],row['SNP_pos'],row['minor_al'],row['major_al'])
             row['rs_ID']= "<a href='http://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?rs=%s'>%s</a><br>" % ((row['RS_num'],) * 2)
             row['TFBS_ID']="<div id=\"%s\"><a onclick='tfbsdata(\"%s\")' href='print_tfbs_data.py?id=%s' target=\"_blank\">tfbs_%s</a></div>" % ((row['TFBS_ID'],) * 4)            
