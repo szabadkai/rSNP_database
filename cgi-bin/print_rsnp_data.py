4d4094437b18cb0d59a741b6dd166f2cd7a340b9
szabadkai <szabadkai@abc.hu>
2014-05-23 08:28:41 +0200

    refresh

--- cgi-bin/print_rsnp_data.py
+++ cgi-bin/print_rsnp_data.py
@@ -44,12 +44,10 @@ with con:
             #     print row['start']-row['SNP_pos']
             # else:
             #     print row['stop']-row['SNP_pos']
-
+            print row.keys()
             row['matrix_id']="<a href='print_matrix.py?id=%s;minor=%s;major=%s'>show matrix</a>" % (row['rs_ID'],row['rs_ID'],row['rs_ID'])
             row['rs_ID']= "<a href='http://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?rs=%s'>%s</a><br>" % ((row['RS_num'],) * 2)
-            row['TFBS_ID']="<div id=\"%s\"><a onclick='tfbsdata(\"%s\")' href='print_tfbs_data.py?id=%s' target=\"_blank\">tfbs_%s</a></div>" % ((row['TFBS_ID'],) * 4)
-            print 'pina!'
-            
+            row['TFBS_ID']="<div id=\"%s\"><a onclick='tfbsdata(\"%s\")' href='print_tfbs_data.py?id=%s' target=\"_blank\">tfbs_%s</a></div>" % ((row['TFBS_ID'],) * 4)            
             print "<tr>"
             for col in header_order:
                 print "<th>%s</th>" % row[col]
