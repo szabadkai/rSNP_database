8da13118e09f7225b59656eb8513bfd65ead6c5b
szabadkai <szabadkai@abc.hu>
2014-05-23 08:27:35 +0200

    refresh

--- cgi-bin/print_rsnp_data.py
+++ cgi-bin/print_rsnp_data.py
@@ -48,7 +48,7 @@ with con:
             row['matrix_id']="<a href='print_matrix.py?id=%s;minor=%s;major=%s'>show matrix</a>" % (row['rs_ID'],row['rs_ID'],row['rs_ID'])
             row['rs_ID']= "<a href='http://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?rs=%s'>%s</a><br>" % ((row['RS_num'],) * 2)
             row['TFBS_ID']="<div id=\"%s\"><a onclick='tfbsdata(\"%s\")' href='print_tfbs_data.py?id=%s' target=\"_blank\">tfbs_%s</a></div>" % ((row['TFBS_ID'],) * 4)
-            print row.keys()
+            print 'pina!'
             
             print "<tr>"
             for col in header_order:
