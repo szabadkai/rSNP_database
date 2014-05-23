527a49b292f67721ff1f055623e33d2cf8646096
szabadkai <szabadkai@abc.hu>
2014-05-23 13:20:16 +0200

    refresh

--- cgi-bin/lookup_gene.py
+++ cgi-bin/lookup_gene.py
@@ -15,16 +15,12 @@ print(yate.include_header("Here are your SNP(s), served fresh and hot!"))
 
 with con:
     print '<div class="input_field"><table>'
-    print "<thead><tr>GENES"
-    print "</tr></thead>"
-        
-
     cur = con.cursor(mdb.cursors.DictCursor)
     cur.execute( "SELECT  * FROM    HTTP,TFBS,GENE2TFBS WHERE  GENE2TFBS.gene LIKE '%"+form_data.upper()+"%' AND  GENE2TFBS.TFBS_ID = TFBS.TFBS_ID AND CONCAT_WS('_',TFBS.organism,TFBS.disease,TFBS.experiment)=HTTP.experiment ORDER BY TFBS.organism,TFBS.disease,TFBS.experiment" )
     rows = cur.fetchall()
     
     for row in rows:
-        row['TFBS_ID']="<div id=\"%s\"><a onclick='tfbsdata(\"%s\")' href='print_tfbs_data.py?id=%s'target=\"_blank\">%s</a></div>" % (row['TFBS_ID'],row['TFBS_ID'],row['TFBS_ID'],row['gene'])
+        row['TFBS_ID']="<div id=\"%s\"><a onclick='tfbsdata(\"%s\")' href='print_tfbs_data.py?id=%s'target=\"_blank\">tfbs%s</a></div>" % (row['TFBS_ID'],row['TFBS_ID'],row['TFBS_ID'],row['gene'])
         print "<th>%s</th>" % row['TFBS_ID']
         print "<th>%s %s</th>" % (row['organism'],row['disease'])
         print "<th><a href='%s'>%s<a></th></tr>" % (row['http'],row['TFBS.experiment'])
--- cgi-bin/print_tfbs_data.py
+++ cgi-bin/print_tfbs_data.py
@@ -78,7 +78,7 @@ with con:
 
             row['matrix_id']="<a href='print_matrix.py?id=%s&pos=%s&minor=%s&major=%s'>show matrix</a>" % (row['rs_ID'],pos,row['minor_al'],row['major_al'])
             row['rs_ID']= "<a href='http://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?rs=%s'>%s</a><br>" % ((row['RS_num'],) * 2)
-            row['TFBS_ID']="<div id=\"%s\"><a onclick='tfbsdata(\"%s\")' href='print_tfbs_data.py?id=%s' target=\"_blank\">tfbs_%s</a></div>" % ((row['TFBS_ID'],) * 4)
+            row['TFBS_ID']="<div id=\"%s\"><a onclick='tfbsdata(\"%s\")' href='print_tfbs_data.py?id=%s' target=\"_blank\">tfbs%s</a></div>" % ((row['TFBS_ID'],) * 4)
             print "<tr>"
             for col in header_order:
                 print "<th>%s</th>" % row[col]
