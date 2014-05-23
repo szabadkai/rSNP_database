8325d8c90d31c2c6d05c3444e5f059ea9c2e99d8
szabadkai <szabadkai@abc.hu>
2014-05-23 12:52:04 +0200

    refresh

--- cgi-bin/print_tfbs_data.py
+++ cgi-bin/print_tfbs_data.py
@@ -72,7 +72,7 @@ with con:
             
         for row in rows:
             if row['strand']=='-':
-                pos = row['SNP_pos']-row['stop']
+                pos = row['SNP_pos'],row['stop'],row['start']
             else: 
                 pos = row['SNP_pos']-row['start']
 
