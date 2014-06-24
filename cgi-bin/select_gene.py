2f530b3fb643aab932d5e5ebf742da55b6b997ec
szabadkai <szabadkai@abc.hu>
2014-06-24 16:16:52 +0200

    m

--- cgi-bin/select_gene.py
+++ cgi-bin/select_gene.py
@@ -15,7 +15,7 @@ print(yate.include_header("Here are your SNP(s), served fresh and hot!"))
 genes = GenePic()
 
 with con:
-    print '<div class="left fixed800">'
+    print '<div class="left fixed80perc">'
     cur = con.cursor(mdb.cursors.DictCursor)
     cur.execute( """SELECT DISTINCT( GENE.alt_name), GENE.gene_id    
     				FROM    GENE    
