6cc430424d0133c5c66ef93e860f616a138b9405
szabadkai <szabadkai@abc.hu>
2014-06-24 14:47:59 +0200

    genelookup

--- cgi-bin/lookup_gene.py
+++ cgi-bin/lookup_gene.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 
 
 import MySQLdb as mdb
 import cgi
@@ -21,7 +21,7 @@ with con:
                                 AND  GENE.gene_id = GENE2TFBS.gene_id
                                 AND GENE2TFBS.TFBS_ID = TFBS.TFBS_ID 
                                 AND CONCAT_WS('_','hs',TFBS.disease,TFBS.experiment)=HTTP.experiment 
-                                ORDER BY TFBS.organism,TFBS.disease,TFBS.experiment""" % (form_data.upper()))
+                                ORDER BY TFBS.organism,TFBS.disease,TFBS.experiment ORDER by GENE.gene_id""" % (form_data.upper()))
     rows = cur.fetchall()
     
     for row in rows:
