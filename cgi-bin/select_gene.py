04be2ae47e8d9f5462aadbc998409614680fc7cf
szabadkai <szabadkai@abc.hu>
2014-06-24 15:25:59 +0200

    m

---
+++ cgi-bin/select_gene.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python 
+
+import MySQLdb as mdb
+import cgi
+import yate
+from GenePic import *
+
+con = mdb.connect('genome', 'rsnp', 'RSNP', 'testdb');
+
+form_data = cgi.FieldStorage().getvalue('gene')
+
+print(yate.start_response())
+print(yate.include_header("Here are your SNP(s), served fresh and hot!"))  
+
+genes = GenePic()
+
+with con:
+    print '<div class="input_field">'
+    cur = con.cursor(mdb.cursors.DictCursor)
+    cur.execute( """SELECT DISTINCT( GENE.alt_name), GENE.gene_id    
+    				FROM    GENE    
+    				WHERE  GENE.alt_name LIKE '%%%s%%';""" % (form_data.upper()))
+
+    rows = cur.fetchall()
+    for row in rows:
+    	print("<a href='lookup_gene.py?gene_id=%s'>%s,</a>" % (row['alt_name'],row['gene_id']),
+
+
+    print("</div>")
+    genes.drawpic()
+print(yate.include_footer(""))
