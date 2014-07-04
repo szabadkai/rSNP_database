b9368abf94a5a3ef6061545f00e092e8b6fdc358
szabadkai <szabadkai@abc.hu>
2014-07-04 11:37:15 +0200

    2

--- js/rsnp.js
+++ js/rsnp.js
@@ -1,14 +1,2 @@
-// function tfbsdata(tfbsid){
-//    div = document.getElementById(tfbsid);
-//    div.style.width  = 200 + "px";
-//    div.style.height = 200 + "px";
-// }
-
-//window.onload = function(){
-//  alert("It is working");
-//}
-$(".tiptext").mouseover(function() {
-    $(this).children(".description").show();
-}).mouseout(function() {
-    $(this).children(".description").hide();
+$(document).ready(function(){
 });
