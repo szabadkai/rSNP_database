79befe84c0aae881a90c5a6cde6bc5fb8bb4e9be
szabadkai <szabadkai@abc.hu>
2014-07-04 16:00:52 +0200

    2

--- cgi-bin/print_rsnp_data.py
+++ cgi-bin/print_rsnp_data.py
@@ -56,51 +56,45 @@ for form_data in split_input(field):
         
         print("</table></div><br>")
 
-        #x= "<a href='ortho_fasta.py?peak=%s' download='%s.fa'>download peak orthologs</a><br>" % (rows[0]['peak'],rows[0]['peak'])
+    #     #x= "<a href='ortho_fasta.py?peak=%s' download='%s.fa'>download peak orthologs</a><br>" % (rows[0]['peak'],rows[0]['peak'])
 
        
-        header_order = ['rs_ID','major_al', 'minor_al', 'freq_major', 'freq_min','rSNP_phastcons','orto_bases','matrix_id'] 
-        header={'rs_ID' :'SNP ID' , 'freq_major':'F Major', 'freq_min':'F Minor','major_al':'MAJOR allele', 'minor_al':'MINOR allele','rSNP_phastcons' :'SNP phascons score','orto_bases':'Orthologs','matrix_id':'MATRIX'}
+    #     header_order = ['rs_ID','major_al', 'minor_al', 'freq_major', 'freq_min','rSNP_phastcons','orto_bases','matrix_id'] 
+    #     header={'rs_ID' :'SNP ID' , 'freq_major':'F Major', 'freq_min':'F Minor','major_al':'MAJOR allele', 'minor_al':'MINOR allele','rSNP_phastcons' :'SNP phascons score','orto_bases':'Orthologs','matrix_id':'MATRIX'}
         
-        cur = con.cursor(mdb.cursors.DictCursor)
-        cur.execute(""" SELECT RS.* ,TFBS.TFBS_ID,TFBS.matrix_id, TFBS.start, TFBS.stop, TFBS.strand
-                        FROM TFBS,RS 
-                        WHERE RS.rs_ID='%s' AND TFBS.TFBS_ID = RS.TFBS_ID""" % form_data)
+    #     cur = con.cursor(mdb.cursors.DictCursor)
+    #     cur.execute(""" SELECT RS.* ,TFBS.TFBS_ID,TFBS.matrix_id, TFBS.start, TFBS.stop, TFBS.strand
+    #                     FROM TFBS,RS 
+    #                     WHERE RS.rs_ID='%s' AND TFBS.TFBS_ID = RS.TFBS_ID""" % form_data)
 
-        rows = cur.fetchall()
+    #     rows = cur.fetchall()
 
-        if len(rows)>0:
-            print '<div class="rsnp_view"><table><tr>'
-            for col in header_order:
-                print "<td>%s</td>" % header[col]
-            print "</tr>"
+    #     if len(rows)>0:
+    #         print '<div class="rsnp_view"><table><tr>'
+    #         for col in header_order:
+    #             print "<td>%s</td>" % header[col]
+    #         print "</tr>"
+    #         for row in rows:
+    #             if row['strand']=='-':
+    #                 pos = row['stop']-row['SNP_pos']
+    #             else:
+    #                 pos = row['SNP_pos']-row['start']
 
-            
-                
-            for row in rows:
-                if row['strand']=='-':
-                    pos = row['stop']-row['SNP_pos']
-                else:
-                    pos = row['SNP_pos']-row['start']
-
-                row['matrix_id']="<a href='print_matrix.py?id=%s&pos=%s&minor=%s&major=%s'>show matrix</a>" % (row['rs_ID'],pos,row['minor_al'],row['major_al'])
-                row['rs_ID']= "<a href='http://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?rs=%s'>%s</a><br>" % ((row['RS_num'],) * 2)
-                row['TFBS_ID']="<div id=\"%s\"><a onclick='tfbsdata(\"%s\")' href='print_tfbs_data.py?id=%s' target=\"_blank\">tfbs%s</a></div>" % ((row['TFBS_ID'],) * 4)
-                temp=[]
-                count=0
-                for letter in row['orto_bases']:
-                    temp.append("<a href='#' title='%s'>%s</a>" % (organisms[count],letter))
-                    count += 1
-                row['orto_bases'] = ''.join(temp) 
-                print "<tr>"
-                for col in header_order:
-                    print "<td>%s</td>" % row[col]
-                print "</tr>"
-            print("</table></div>")
-        else:
-            print"No SNP in this TFBS"
-    #print "<br>"+x
+    #             row['matrix_id']="<a href='print_matrix.py?id=%s&pos=%s&minor=%s&major=%s'>show matrix</a>" % (row['rs_ID'],pos,row['minor_al'],row['major_al'])
+    #             row['rs_ID']= "<a href='http://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?rs=%s'>%s</a><br>" % ((row['RS_num'],) * 2)
+    #             row['TFBS_ID']="<div id=\"%s\"><a onclick='tfbsdata(\"%s\")' href='print_tfbs_data.py?id=%s' target=\"_blank\">tfbs%s</a></div>" % ((row['TFBS_ID'],) * 4)
+    #             temp=[]
+    #             count=0
+    #             for letter in row['orto_bases']:
+    #                 temp.append("<a href='#' title='%s'>%s</a>" % (organisms[count],letter))
+    #                 count += 1
+    #             row['orto_bases'] = ''.join(temp) 
+    #             print "<tr>"
+    #             for col in header_order:
+    #                 print "<td>%s</td>" % row[col]
+    #             print "</tr>"
+    #         print("</table></div>")
+    #     else:
+    #         print"No SNP in this TFBS"
+    # #print "<br>"+x
 print(yate.include_footer({""}))
-
-
-
