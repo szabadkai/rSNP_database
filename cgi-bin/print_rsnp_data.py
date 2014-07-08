c257edda108421a789b75e2a9fc3a7c90eaf8df2
szabadkai <szabadkai@abc.hu>
2014-07-08 11:34:38 +0200

    unity

--- cgi-bin/print_rsnp_data.py
+++ cgi-bin/print_rsnp_data.py
@@ -35,7 +35,8 @@ with con:
         cur = con.cursor(mdb.cursors.DictCursor)
         cur.execute(""" SELECT RS.* ,TFBS.TFBS_ID,TFBS.matrix_id, TFBS.start, TFBS.stop, TFBS.strand
                         FROM TFBS,RS 
-                        WHERE TFBS.TFBS_ID= (SELECT TFBS_ID FROM TFBS WHERE TFBS.TFBS_ID = RS.TFBS_ID AND RS.num = '%s') AND TFBS.TFBS_ID = RS.TFBS_ID ;""" % form_data)
+                        WHERE TFBS.TFBS_ID = (SELECT TFBS_ID FROM TFBS WHERE TFBS.TFBS_ID = RS.TFBS_ID AND RS.num = '%s') 
+                        AND TFBS.TFBS_ID = RS.TFBS_ID ;""" % form_data)
 
         rows = cur.fetchall()
 
