813a6d4aa0ebffd335889d183a9e31e4631775a4
szabadkai <szabadkai@abc.hu>
2014-07-09 13:50:43 +0200

    functions1

--- cgi-bin/TFBS_tools.py
+++ cgi-bin/TFBS_tools.py
@@ -3,8 +3,8 @@ def print_tfbs(tfbs_ID):
     con = mdb.connect('genome', 'rsnp', 'RSNP', 'testdb');
     from mysql_open import *
     organisms=('hg19','panTro2','gorGor1','ponAbe2','rheMac2','papHam1','calJac1','tarSyr1','micMur1','otoGar1','tupBel1','mm9','rn4','dipOrd1','cavPor3','speTri1','oryCun2','ochPri2','vicPac1','turTru1','bosTau4','equCab2','felCat3','canFam2','myoLuc1','pteVam1','eriEur1','sorAra1','loxAfr3','proCap1','echTel1','dasNov2','choHof1','macEug1','monDom5','ornAna1')
-    header_order_tfbs = ['TFBS_ID','ORTHOLOGS.peak','de_novo_motif','chr','start','stop','similar_TFBS', 'target_perc','p','celltype','antibody','GEO','orthologs','snp_count']
-    header_tfbs = {'celltype':'disease','antibody':'experiment','snp_count':'SNP count','GEO':'GEO','orthologs':'peak orthologs','TFBS_ID':'TFBS','ORTHOLOGS.peak':'PEAK','de_novo_motif':'motif', 'chr':'chr','start':'start','similar_TFBS':'similar_TFBS' ,'stop':'stop','target_perc':'target%','p':'P'}
+    header_order_tfbs = ['TFBS_ID','ORTHOLOGS.peak','de_novo_motif','chr','start','stop','similar_TFBS', 'target_perc','p','disease','experiment','GEO','orthologs','snp_count']
+    header_tfbs = {'disease':'celltype','experiment':'antibody','snp_count':'SNP count','GEO':'GEO','orthologs':'peak orthologs','TFBS_ID':'TFBS','ORTHOLOGS.peak':'PEAK','de_novo_motif':'motif', 'chr':'chr','start':'start','similar_TFBS':'similar_TFBS' ,'stop':'stop','target_perc':'target%','p':'P'}
     print"<script src='../js/tfbs.js'></script>"
 
     with con:
