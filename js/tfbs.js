2798a9493b8ee30cb577772776ab433c837e62cf
szabadkai <szabadkai@abc.hu>
2014-07-09 15:13:36 +0200

    functions1

--- js/tfbs.js
+++ js/tfbs.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
 	$(".rsnp_view").hide();
 
 	$(".tfbs_view").on("click", function(){
-		$(this).find("tr").last().find(".rsnp_view").slideToggle();
+		$(".rsnp_view").slideToggle();
 	});
 });
