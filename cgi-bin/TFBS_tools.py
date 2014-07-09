28d421c1430b0b1cd1278cc39ad23688b0f1fab6
szabadkai <szabadkai@abc.hu>
2014-07-09 13:19:36 +0200

    functions1

--- cgi-bin/TFBS_tools.py
+++ cgi-bin/TFBS_tools.py
@@ -13,7 +13,7 @@ def print_tfbs(tfbs_ID):
                         FROM TFBS,RS
                         WHERE TFBS.TFBS_ID='%s' AND TFBS.TFBS_ID = RS.TFBS_ID""" % tfbs_ID)
 
-        snp_count = cur.fetchone()['COUNT(RS.rs_ID']
+        snp_count = cur.fetchone()['COUNT(RS.rs_ID)']
 
         print '<div class="tfbs_view"><table>'
         cur.execute(""" SELECT * FROM TFBS,ORTHOLOGS,HTTP 
