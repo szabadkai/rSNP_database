e80451eec6c7b0c25a02f19b6f2e2e3af48945f1
szabadkai <szabadkai@abc.hu>
2014-07-10 11:39:57 +0200

    functions1

--- cgi-bin/print_rsnp_data.py
+++ cgi-bin/print_rsnp_data.py
@@ -30,7 +30,7 @@ with con:
     tfbs = []
     for form_data in split_input(field):
         cur = con.cursor(mdb.cursors.DictCursor)
-        cur.execute(""" SELECT DISTINCT RS.TFBS_ID FROM TFBS, RS WHERE TFBS.TFBS_ID = (SELECT DISTINCT TFBS_ID FROM RS WHERE rs_num = '%s') AND TFBS.TFBS_ID = RS.TFBS_ID;""" % form_data)
+        cur.execute(""" SELECT DISTINCT RS.TFBS_ID FROM TFBS, RS WHERE TFBS.TFBS_ID IN (SELECT DISTINCT TFBS_ID FROM RS WHERE rs_num = '%s') AND TFBS.TFBS_ID = RS.TFBS_ID;""" % form_data)
         rows = cur.fetchall()
 
         if len(rows)>0:
